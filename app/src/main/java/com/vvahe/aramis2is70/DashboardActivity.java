ce876a2c87f072dc9eafdfdbcc68351d49d2d495
vVahe <Vahe@hotmail.nl>
2018-02-19 20:23:21 +0100

    Initial commit

---
+++ app/src/main/java/com/vvahe/aramis2is70/DashboardActivity.java
@@ -0,0 +1,18 @@
+package com.vvahe.aramis2is70;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    private TextView txtHome;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        txtHome = (TextView) findViewById(R.id.txtHome);
+    }
+}

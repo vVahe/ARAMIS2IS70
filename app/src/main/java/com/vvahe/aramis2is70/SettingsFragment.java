4819d83a995498f2ad2a7b9e441be2084f11d876
ThomasDerwig <Thomas_Derwig@outlook.com>
2018-03-13 14:42:41 +0100

    Settings page frontend

--- app/src/main/java/com/vvahe/aramis2is70/SettingsFragment.java
+++ app/src/main/java/com/vvahe/aramis2is70/SettingsFragment.java
@@ -6,12 +6,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SettingsFragment extends Fragment {
+    private static SeekBar seekBar;
+    private static TextView textView;
+    private static Spinner spinner;
 
 
     public SettingsFragment() {
@@ -23,7 +30,67 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        // set all of the elements
+        seekBar = (SeekBar)view.findViewById(R.id.searchradius_seekBar);
+        textView = (TextView)view.findViewById(R.id.seekbar_text);
+        spinner = (Spinner)view.findViewById(R.id.map_filter_spinner);
+        // methods for data handeling of seekBar and Spinner
+        seekBar();
+        spinner();
+        return view;
     }
+
+    public void spinner() { // sets the Array of strings from resources as options for spinner
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.filter_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+    public void seekBar(){  // attaches textview with progress to the thumb of the seekBar
+        textView.setText(Integer.toString(seekBar.getProgress()));
+
+        //calculate the x coördinate of the thumb of the seekbar
+        double percent = seekBar.getProgress() / (double) seekBar.getMax();
+        int offset = seekBar.getThumbOffset();
+        int seekWidth = seekBar.getWidth();
+        int val = (int) Math.round(percent * (seekWidth - 2 * offset));
+        int labelWidth = textView.getWidth();
+        textView.setX(offset + seekBar.getX() + val   // set x of textview to that of seekBar thumb
+                - Math.round(percent * offset)
+                - Math.round(percent * labelWidth/2));
+
+        seekBar.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    @Override // keep updating x coordinate of textview to that of the seekBar
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        textView.setText(Integer.toString(progress));
+                        double percent = progress / (double) seekBar.getMax();
+                        int offset = seekBar.getThumbOffset();
+                        int seekWidth = seekBar.getWidth();
+                        int val = (int) Math.round(percent * (seekWidth - 2 * offset));
+                        int labelWidth = textView.getWidth();
+                        textView.setX(offset + seekBar.getX() + val
+                        - Math.round(percent * offset)
+                        - Math.round(percent * labelWidth/2));
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+
+                    }
+                }
+        );
+
+    }
+
+
 
 }

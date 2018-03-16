16cf0b041515858ac074e21d5e25903286c506e4
vVahe <Vahe@hotmail.nl>
2018-03-16 14:30:54 +0100

    moved login/authentication code from dashboardfrag. to mainactivity fixed register screen, logout button now works

--- app/src/main/java/com/vvahe/aramis2is70/DashboardFragment.java
+++ app/src/main/java/com/vvahe/aramis2is70/DashboardFragment.java
@@ -1,8 +1,5 @@
 package com.vvahe.aramis2is70;
 
-
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,29 +7,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DashboardFragment extends Fragment {
-
-    private TextView txtUser;
-    private Button btnLogout;
-    private String userName;
-
-    private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
-    private DatabaseReference mDatabase;
 
     public DashboardFragment() {
         // Required empty public constructor
@@ -48,82 +29,10 @@ public class DashboardFragment extends Fragment {
     /** put code that should run on start up in onViewCreated */
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        btnLogout = (Button) getView().findViewById(R.id.btnLogout);
-        txtUser = (TextView) getView().findViewById(R.id.txtName);
 
-        mDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
-        mAuth = FirebaseAuth.getInstance();
-
-        /** checks if user is logged in not*/
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-
-                /** FirebaseAuth returns result whether logged in or not
-                 * if user is not logged in redirect to login page using intent*/
-                if (firebaseAuth.getCurrentUser() == null) {
-
-                    Intent loginIntent = new Intent(getActivity(), LoginActivity.class);
-
-                    /** prevents user from going back once logged in, they will have to use logout button*/
-                    loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
-                    startActivity(loginIntent);
-
-                } else { /** user is logged in so display username on screen */
-
-                    getUserName();
-
-                }
-            }
-        };
-
-        btnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                logout();
-            }
-        });
     }
 
-    /** Get username of currently logged in user and set on TextView*/
-    private void getUserName() {
-
-        /** get user id of current user*/
-        final String user_id = mAuth.getCurrentUser().getUid();
 
 
-        mDatabase.child(user_id).getRef().addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-
-                userName = dataSnapshot.child("firstName").getValue().toString();
-                txtUser.setText(userName);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {}
-        });
-    }
-
-    /** Logout user*/
-    private void logout() {
-        mAuth.signOut();
-
-//        Intent logoutIntent = new Intent(, LoginActivity.class);
-//
-//        /** prevents user from going back once logged in, they will have to use logout button*/
-//        logoutIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//
-//        startActivity(logoutIntent);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        mAuth.addAuthStateListener(mAuthListener);
-    }
 
 }
--- app/src/main/java/com/vvahe/aramis2is70/MainActivity.java
+++ app/src/main/java/com/vvahe/aramis2is70/MainActivity.java
@@ -1,36 +1,63 @@
 package com.vvahe.aramis2is70;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView bNavView;
     private FrameLayout frameLayout;
+    private Button logoutBtn;
+    private static final String TAG = "MainActivity";
 
     private DashboardFragment dashboardFragment;
     private MapFragment mapFragment;
     private ChatFragment chatFragment;
     private SettingsFragment settingsFragment;
 
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
+        mAuth = FirebaseAuth.getInstance();
+
         bNavView = findViewById(R.id.mainNav);
         frameLayout = findViewById(R.id.mainFrame);
+        logoutBtn = findViewById(R.id.logoutBtn);
 
         dashboardFragment = new DashboardFragment();
         mapFragment = new MapFragment();
         chatFragment = new ChatFragment();
         settingsFragment = new SettingsFragment();
+
+        checkUserLogin();
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
 
         /** set first fragment to be displayed on startup*/
         setFragment(dashboardFragment);
@@ -66,6 +93,37 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public void logout() {
+        Log.i(TAG, "logged out");
+        mAuth.signOut();
+        Intent logoutIntent = new Intent(MainActivity.this, LoginActivity.class);
+
+        /** prevents user from going back once logged in, they will have to use logout button*/
+        logoutIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        startActivity(logoutIntent);
+    };
+
+    private void checkUserLogin() {
+        /** checks if user is logged in not*/
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                /** FirebaseAuth returns result whether logged in or not
+                 * if user is not logged in redirect to login page using intent*/
+                if (firebaseAuth.getCurrentUser() == null) {
+
+                    Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+
+                    /** prevents user from going back once logged in, they will have to use logout button*/
+                    loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(loginIntent);
+                }
+            }
+        };
+    }
+
     /** switches between fragments */
     private void setFragment(Fragment fragment) {
 
@@ -73,6 +131,13 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.replace(R.id.mainFrame, fragment);
         fragmentTransaction.commit();
 
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        mAuth.addAuthStateListener(mAuthListener);
     }
 
 }

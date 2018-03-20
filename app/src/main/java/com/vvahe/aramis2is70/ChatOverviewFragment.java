8d65b2d680b1db9bcd630fadb4df2fe437dbc8dc
s159102 <33961630+s159102@users.noreply.github.com>
2018-03-20 22:02:50 +0100

    Revert "Revert "chat werkt, met test knop""
    This reverts commit 5450e08463af1eaeca421b4b0f82d795f7732694.

--- app/src/main/java/com/vvahe/aramis2is70/ChatOverviewFragment.java
+++ app/src/main/java/com/vvahe/aramis2is70/ChatOverviewFragment.java
@@ -68,8 +68,8 @@ public class ChatOverviewFragment extends Fragment {
         if (context instanceof OnListFragmentInteractionListener) {
             mListener = (OnListFragmentInteractionListener) context;
         } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnListFragmentInteractionListener");
+            //throw new RuntimeException(context.toString()
+                    //+ " must implement OnListFragmentInteractionListener");
         }
     }
 

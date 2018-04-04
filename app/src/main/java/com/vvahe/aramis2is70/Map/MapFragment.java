e5336eff5972b624c1eb90477ca6169090217db0
Niels <theniels17@users.noreply.github.com>
2018-04-04 11:31:59 +0200

    start map focused on campus

--- app/src/main/java/com/vvahe/aramis2is70/Map/MapFragment.java
+++ app/src/main/java/com/vvahe/aramis2is70/Map/MapFragment.java
@@ -115,8 +115,8 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         enableMyLocation();
 
         CameraPosition cameraPosition = new CameraPosition.Builder()
-                .target(new LatLng(userObj.locationX, userObj.locationY))      // Sets the center of the map to location user
-                .zoom(16)                   // Sets the zoom
+                .target(new LatLng(51.4484, 5.4902))      // Sets the center of the map to location user
+                .zoom(15)                   // Sets the zoom
                 .bearing(0)                // Sets the orientation of the camera to east
                 .tilt(40)                   // Sets the tilt of the camera to 30 degrees
                 .build();                   // Creates a CameraPosition from the builder
